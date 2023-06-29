51eb49f90381ac64307418bef68aab1a9674f919
Jadest13 <jadest13x03@gmail.com>
2023-06-29 14:53:10 +0900

    pythonanywhere prepare

--- github-union-badge/settings.py
+++ github-union-badge/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +25,7 @@ SECRET_KEY = 'django-insecure-d4cwk4=zv#mhinbsh$*k&lnsb&8_g3xakxkdm5(wp_+gtnx=$6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com', '.herokuapp.com']
 
 
 # Application definition
